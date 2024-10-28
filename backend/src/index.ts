b85760fcef04391e56c3afa86ce3a55c84a1a9e8
Gonzalo Fernandez <ezekyel34@gmail.com>
2024-10-28 10:06:24 -0300

    configuración MongoDB mongoose Index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,25 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import mongoose from 'mongoose';
+
 
 const app = express();
 app.use( express.json() )
 app.use( cors() );
 
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
+    .then(() => console.log('Connected to the database'))
+    .catch(err => {
+        console.error('Error connecting to the database:', err.message);
+        process.exit(1); 
+    });
+
+
 app.get('/test', async (req: Request, res: Response) => {
     res.json({ message: 'Holaaaa!!!' });
 });
 
-app.listen(7000, () => {
-    console.log('Server starte on localhost:7000')
+app.listen(6000, () => {
+    console.log('Server started on localhost:6000')
 })
