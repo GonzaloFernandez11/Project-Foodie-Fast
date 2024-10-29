5aa390ee71e05836c26a2d544a52b8674ad358da
Gonzalo Fernandez <ezekyel34@gmail.com>
2024-10-29 15:34:55 -0300

    layout & header init

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+
+const Header = () => {
+    return (
+        <div className="border-b-2 border-b-orange-500 py-6">
+            <div className="container mx-auto flex justify-between items-center">
+                <Link 
+                to="/" 
+                className="text-3x1 font-bold tracking-tight text-orange-500">
+                    FoodieFast.com
+                </Link>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
+
+// Reemplazar el color Orange por otro, o sino el fondo por un gris oscuro.
+// En el Link podría agregar una imagen también.
