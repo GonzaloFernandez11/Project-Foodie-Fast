d5f510128ef9c0437583c266ff51efab47846e21
Gonzalo Fernandez <ezekyel34@gmail.com>
2024-11-01 09:15:54 -0300

    config global styles

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -2,11 +2,11 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
     return (
-        <div className="border-b-2 border-b-orange-500 py-6">
+        <div className="border-b-2 border-b-pink-600 py-6">
             <div className="container mx-auto flex justify-between items-center">
                 <Link 
                 to="/" 
-                className="text-3x1 font-bold tracking-tight text-orange-500">
+                className="text-3x1 font-bold tracking-tight text-pink-600">
                     FoodieFast.com
                 </Link>
             </div>
--- frontend/src/layouts/layout.tsx
+++ frontend/src/layouts/layout.tsx
@@ -9,7 +9,7 @@ const Layout = ({ children }: Props) => {
         <div className="flex flex-col min-h-screen">
             <Header />
 
-            <div className="contaner mx-auto flex-1 py-10">{children}</div>
+            <div className="container mx-auto flex-1 py-10">{children}</div>
         </div>
     );
 };
