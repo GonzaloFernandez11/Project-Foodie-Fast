5bc8d7b759fe84b73d311188aa393f9b3b1c4c3b
Gonzalo Fernandez <ezekyel34@gmail.com>
2024-11-01 21:31:50 -0300

    MainNav ready

---
+++ frontend/src/components/MainNav.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./ui/button";
+
+const MainNav = () => {
+    return(
+        <Button variant="ghost" className="font-bold hover:text-white hover:bg-pink-600">
+            Log In
+        </Button>
+    )
+}
+
+export default MainNav;
