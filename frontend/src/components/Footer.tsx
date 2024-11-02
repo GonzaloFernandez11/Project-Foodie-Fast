65d4d3dfdecf04c033f88147aae2a8051dda0a0a
Gonzalo Fernandez <ezekyel34@gmail.com>
2024-11-02 02:43:18 -0300

    footer integration

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return(
         <div className="bg-pink-600 py-10">
-            <div className="container max-auto flex flex-col md:flex-row justify-between items-center">
+            <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
                 <span className="text-3xl font-bold tracking-tight">
                     FoodieFast.com
                 </span>
