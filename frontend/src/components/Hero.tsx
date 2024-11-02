072d80d194d88b2adaa49d66a113e183721c971a
Gonzalo Fernandez <ezekyel34@gmail.com>
2024-11-01 23:48:55 -0300

    Hero creation - images

---
+++ frontend/src/components/Hero.tsx
@@ -0,0 +1,17 @@
+const Hero = () => {
+    return(
+        <div>
+            <img src="../assets/hero.png"/>
+        </div>
+    )
+    
+}
+
+export default Hero;
+
+
+// Buscar otras imágenes.
+// Recurso de imágenes en: Unsplash, Pexels, Pixabay, Foodiesfeed
+// Para edición de imágenes: GIMP, Photopea, Canva, Pixlr, Krita, Fotor   \assets\hero.png
+
+// Add carousels or sliders, introduce the info in an attractive way
