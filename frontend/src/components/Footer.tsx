c6d0df6b0da97805f5eb497f3baa29e2ef2e0016
Gonzalo Fernandez <ezekyel34@gmail.com>
2024-11-02 02:27:07 -0300

    Footer created

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+    return(
+        <div className="bg-pink-600 py-10">
+            <div className="container max-auto flex flex-col md:flex-row justify-between items-center">
+                <span className="text-3xl font-bold tracking-tight">
+                    FoodieFast.com
+                </span>
+                <span className="font-bold tracking-tight flex gap-4">
+                    <span>Privacy Policy</span>
+                    <span>Terms of Service</span>
+                </span>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
