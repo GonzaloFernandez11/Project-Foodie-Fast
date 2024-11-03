3d569d0334776e27b366d1d4925d637d3069e5e9
Gonzalo Fernandez <ezekyel34@gmail.com>
2024-11-03 07:56:11 -0300

    UsernameMenu created and config

--- frontend/src/components/UsernameMenu.tsx
+++ frontend/src/components/UsernameMenu.tsx
@@ -1,6 +1,35 @@
+// import { DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger, DropdownMenuItem } from "./ui/dropdown-menu";
+import { CircleUserRound } from "lucide-react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Link } from "react-router-dom";
+import { Separator } from "./ui/separator";
+import { Button } from "./ui/button";
+
 const UsernameMenu = () => {
+    const { user, logout } = useAuth0();
     return(
-        <></>
+        <DropdownMenu>
+            <DropdownMenuTrigger className="flex items-center px-3 font-bold hover:text-pink-600 gap-2">
+                <CircleUserRound className="text-pink-600"/>
+                {user?.email}
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <DropdownMenuItem>
+                <Link to="/user-profile" className="font-bold hover:text-pink-600">
+                    User Profile
+                </Link>
+                </DropdownMenuItem>
+                <Separator />
+                <DropdownMenuItem>
+                    <Button onClick={() => logout()} 
+                    className="flex flex-1 font-bold bg-pink-600"
+                    >
+                        Log Out
+                    </Button>
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
     )
 
 }
