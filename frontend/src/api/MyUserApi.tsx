46cb8c29c2211b237b678c7503485deb1d6a88c1
Gonzalo Fernandez <ezekyel34@gmail.com>
2024-11-12 16:16:14 -0300

    useGetMyUser hook in MyUserApi created to manage the GET requests

--- frontend/src/api/MyUserApi.tsx
+++ frontend/src/api/MyUserApi.tsx
@@ -1,8 +1,42 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import { useMutation } from "react-query";
+import { useMutation, useQuery } from "react-query";
 import { toast } from "sonner";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+
+export const useGetMyUser = () => {
+    const { getAccessTokenSilently } = useAuth0();
+
+    const getMyUserRequest = async () => {
+        const accessToken = await getAccessTokenSilently();
+
+        const response = await fetch(`${API_BASE_URL}/api/my/user`, {
+            "method": "GET",
+            "headers": {
+                Authorization: `Bearer ${accessToken}`,
+                "Content-Type": "aplication/json",
+            }
+        });
+        if(!response.ok) {
+            throw new Error(`Failed to fetch user`);
+        }
+        response.json();
+    }
+
+    const {
+        data: currentUser, 
+        error, 
+        isLoading 
+    } = useQuery("fetchCurrentUser", getMyUserRequest);
+
+    if(error) {
+        toast.error(error.toString());
+    }
+
+    return { currentUser, isLoading };
+}; 
+
 
 type CreateUserRequest = {
     auth0Id: string;
@@ -92,4 +126,4 @@ export const useUpdateMyUser = () => {
     };
 
     return { updateUser, isLoading }
-}
+};
