b303b9cb0c74be8306164678926425ee5884ff44
Gonzalo Fernandez <ezekyel34@gmail.com>
2024-11-12 14:10:12 -0300

    controller for get the user info created - added to MyUserRoutes

--- backend/src/controllers/MyUserController.ts
+++ backend/src/controllers/MyUserController.ts
@@ -1,6 +1,25 @@
 import { Request, Response } from "express";
 import User from "../models/user";
 
+
+const getCurrentUser = async (req: Request, res: Response) => {
+
+    try {
+        const currentUser = await User.findOne({ _id: req.userId });
+        if(!currentUser) {
+            res.status(404).json({ message: 'User not found' });
+            return;
+        }
+
+        res.json(currentUser);
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'Something went wrong' });
+        return;
+    }
+}
+
 const createCurrentUser = async (req: Request, res: Response) => {
     
     try {
@@ -50,6 +69,7 @@ const updateCurrentUser = async (req: Request, res: Response) => {
 }
 
 export default {
+    getCurrentUser,
     createCurrentUser,
     updateCurrentUser,
 }
